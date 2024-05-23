272da9eeedf6dddeed4bd9b59e5a73a718b10e6a
wychoi <soc09072@gmail.com>
2024-05-23 13:59:49 +0900

    Add: loginpage init

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const LoginPageBlock = styled.div`
+
+`;
+
+const LoginPage = () => {
+	/* 로그인 상태라면 /list로 리다이렉트 ... 할까*/
+  return (
+    <LoginPageBlock>
+    <div>
+        로그인 페이지
+	 </div>
+     </LoginPageBlock>
+  );
+};
+
+export default NotFoundErrorPage;
