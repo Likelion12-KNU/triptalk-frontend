d1849f4f7f61a8e9971484d7fca7fe459cc3330f
wychoi <soc09072@gmail.com>
2024-05-23 13:58:24 +0900

    Add: palette 주석에 예비 색상 추가

--- src/lib/styles/palette.js
+++ src/lib/styles/palette.js
@@ -37,6 +37,21 @@ const palette = {
     blue: []
     ,
   };
-  
+  // BLUE: "#3779F1",// 메인 블루
+  // RED: "#D13B44",//빨강
+  // BORDER: "#d3d3d3", // border 회색
+  // WHITE: "#ffffff", // 흰색
+  // LIGHT_GRAY : '#F6F6F6', //밝은 회색
+  // GRAY_BACKGROUND_ALPHA: "#00000080", // 팝업용 투명한 배경 회색
+  // GRAY_BACKGROUND: "#f4f4f4", // 배경 회색
+  // GRAY_LETTER: "#838383", // 글자 회색
+  // BLACK: "#000000", // 검정
+  // SKY: "#E7F1FF", // 하늘색 
+  // GREEN: "#57994B", //초록색
+  // LIGHT_GREEN : '#CEF6D8',//밝은 초록색
+  // CHAT_ME: "#B6D8FD", // 채팅 나
+  // CHAT_YOU: "#EFEFEF", // 채팅 상대방
+  // RED_HIGHLIGHT: "#FF0000", //밝은 빨강 (하이라이트용)
+  // GRAY_SEARCH: "#E7E7E9" // 검색창 회색
   export default palette;
   
