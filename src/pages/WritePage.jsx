b80b5e658f188c5881e559ccb56520ca730be585
wychoi <soc09072@gmail.com>
2024-05-23 14:03:38 +0900

    Add: WritePage init

---
+++ src/pages/WritePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const WritePageBlock = styled.div`
+
+`;
+
+const WritePage = () => {
+  return (
+    <WritePageBlock>
+    <div>
+        WritePage
+	 </div>
+     </WritePageBlock>
+  );
+};
+
+export default WritePage;
