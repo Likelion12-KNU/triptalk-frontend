2fd9d7d574a96b0397162bc7f14daaf147380860
wychoi <soc09072@gmail.com>
2024-05-23 14:02:48 +0900

    Add: PostPage init

---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const PostPageBlock = styled.div`
+
+`;
+
+const PostPage = () => {
+  return (
+    <PostPageBlock>
+    <div>
+        PostPage
+	 </div>
+     </PostPageBlock>
+  );
+};
+
+export default PostPage;
