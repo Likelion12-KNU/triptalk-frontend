b4c11a75474c19e65837ac34c4f3a6ca7d5c29a9
castberry10 <soc09072@gmail.com>
2024-05-26 23:17:40 +0900

    Add: main.jsx에 loadUser, store추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,28 @@ import App from './App.jsx'
 import './index.css'
 import {HelmetProvider} from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
+import { tempSetUser, check } from './modules/user';
 
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return;
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+loadUser();
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
+  </Provider>
 )
