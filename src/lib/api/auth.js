106ed252b63819982c5404ee3aa7ad0cec6995ee
wychoi <soc09072@gmail.com>
2024-05-31 15:13:13 +0900

    Set: auth api수정

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -5,8 +5,8 @@ export const login = ({ username, password }) =>
     client.post('/api/auth/login', { username, password });
   
 // 회원가입
-export const signup = ({ username, password }) =>
-    client.post('/api/auth/signup', { username, password });
+export const signup = ({ username, password, email, nickname }) =>
+    client.post('/api/auth/signup', { username, password, email, nickname});
   
 // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
