a5fab02956ecf9a6befa1318df39f073117e6572
wychoi <soc09072@gmail.com>
2024-05-31 14:00:56 +0900

    Add: dotenv를 통한 API 서버 설정

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-
+import { API_URL } from '../../config';
 
 const client = axios.create({
-    baseURL: 'localhost:4000/',
+    // baseURL: 'localhost:4000/',
+    baseURL: `${API_URL}`,
     // baseURL: 'http://api-server.com/,
     // 필요에 따라 추가 옵션 설정
     // timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar'}
-});
+d});
+
 export default client;
