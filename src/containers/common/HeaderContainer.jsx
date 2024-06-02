b531477ed913d79656924444cff9ff8b796add0c
castberry10 <soc09072@gmail.com>
2024-06-02 23:47:26 +0900

    새로고침 하면 생기는 오류 고침

--- src/containers/common/HeaderContainer.jsx
+++ src/containers/common/HeaderContainer.jsx
@@ -6,15 +6,27 @@ import { authDelete } from '../../modules/auth';
 
 const HeaderContainer = () => {
   // const { user } = useSelector(({ user }) => ({ user: user.nickname }));
-  const { user } = useSelector(({ user }) => ({ user: user.user }));
+  let { user } = useSelector(({ user }) => ({ user: user.user }));
   const dispatch = useDispatch();
   const onLogout = () => {
     dispatch(logout());
     dispatch(authDelete());
   };
+  // console.log(user);
+  // if(user){
+
+  // }else{
+  //   isuser = localStorage.getItem('user');
+  //   console.log(isuser);
+  //   if(!isuser){
+  //     user = isuser;
+  //   }
+  // }
   // console.log("user", user);
   // console.log(1);
-  
+  if(user){
+    user = JSON.parse(user);
+  }
   return <Header user={user} onLogout={onLogout} />;
 };
 
