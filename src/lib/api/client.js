188e9f9f2fc8dfe5164c35f0c1f405e74121d5eb
castberry10 <soc09072@gmail.com>
2024-06-03 02:30:43 +0900

    지나가는 client 삥뜯어서 토큰 넣기

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -10,4 +10,21 @@ const client = axios.create({
     // headers: {'X-Custom-Header': 'foobar'}
 });
 
+// 요청 인터셉터 설정
+client.interceptors.request.use(
+    (config) => {
+      // 로컬스토리지에서 토큰 가져오기
+      const token = localStorage.getItem('token');
+      if (token) {
+        // 토큰이 있으면 요청 헤더에 토큰 추가
+        config.headers.Authorization = `${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      // 요청 오류가 발생하면 그대로 반환
+      return Promise.reject(error);
+    }
+  );
+
 export default client;
